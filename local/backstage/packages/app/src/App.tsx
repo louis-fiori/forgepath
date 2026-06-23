743bdeeda75a44a0230cf26eeaddba45fe2f6481
Louis Fiori <170877473+louis-fiori@users.noreply.github.com>
2026-06-23 14:53:37 +0200

    chore: untrack local/backstage scaffold (generated by make backstage-init)

--- local/backstage/packages/app/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { createApp } from '@backstage/frontend-defaults';
-import catalogPlugin from '@backstage/plugin-catalog/alpha';
-import techdocsPlugin from '@backstage/plugin-techdocs/alpha';
-import { navModule } from './modules/nav';
-
-export default createApp({
-  features: [catalogPlugin, techdocsPlugin, navModule],
-});
